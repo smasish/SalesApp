5720dde1ae5c58ff5f4cbce71d1e9031392a2e7d
smasish <asish_cse@yahoo.com>
2017-10-02 12:34:12 +0600

    volley integrated

---
+++ app/src/main/java/constant/Url.java
@@ -0,0 +1,23 @@
+package constant;
+
+/**
+ * Created by Asish on 23/09/2017.
+ */
+public class Url {
+    public static String BASE_URL = "https://sindabad.com/restapi/index.php/";
+    public static String CATEGORY_URL = "https://sindabad.com/restapi/index.php/search";
+    public static String CUSTOMERS_URL = "https://staging.sindabad.com/restapi/index.php/api/customers";
+    public static String PAYMENT_STATUS_URL = "https://sindabad.com/restapi/index.php/search";
+
+
+    public static final String HOME_DELIVERY = "https://kiksha.com/delivery.html";
+    public static final String RETURN_REFUND = "https://kiksha.com/return.html";
+    public static final String APP_INTRO = "https://kiksha.com/appintro.html";
+    public static final String FAQ = "https://kiksha.com/app-page/faq.html";
+    public static final String PRIVACY = "https://kiksha.com/app-page/privacy.html";
+    public static final String TERMS = "https://kiksha.com/app-page/terms.html";
+    public static final String CONTACT_US = "https://kiksha.com/app-page/contact-us.html";
+
+    public static final String FB_NAME = "Sindabad.com.bd";
+    public static final String KIKSHA_FB = "https://www.facebook.com/" + FB_NAME;
+}
